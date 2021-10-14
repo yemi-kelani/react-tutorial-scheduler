73f7b53860b8998c286309d57f26b9fcefb45088
Yemi Kelani <aak.development@gmail.com>
2021-10-14 14:02:00 -0500

    Modularize part 2

---
+++ src/components/Course.js
@@ -0,0 +1,29 @@
+import { hasConflict, getCourseTerm } from '../utilities/times.js';
+
+const Course = ({ course, selected, setSelected }) => {
+    const isSelected = selected.includes(course);
+    const isDisabled = !isSelected && hasConflict(course, selected);
+    const style = {
+      backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
+    };
+    return (
+      <div className="card m-1 p-2" 
+        style={style}
+        onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
+        <div className="card-body">
+          <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+          <div className="card-text">{ course.title }</div>
+        </div>
+      </div>
+    );
+};
+
+const toggle = (x, lst) => (
+    lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
+);
+   
+const getCourseNumber = course => (
+    course.id.slice(1, 4)
+);
+
+export default Course;
--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { hasConflict, getCourseTerm, terms } from '../utilities/times.js';
+import { getCourseTerm, terms } from '../utilities/times.js';
+import Course from './Course.js';
 
 const TermButton = ({term, setTerm, checked}) => (
     <>
@@ -40,31 +41,5 @@ const TermButton = ({term, setTerm, checked}) => (
       </>
     );
   };
-  
-  const toggle = (x, lst) => (
-    lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
-  );
-  
-  const Course = ({ course, selected, setSelected }) => {
-    const isSelected = selected.includes(course);
-    const isDisabled = !isSelected && hasConflict(course, selected);
-    const style = {
-      backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
-    };
-    return (
-      <div className="card m-1 p-2" 
-        style={style}
-        onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
-        <div className="card-body">
-          <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-          <div className="card-text">{ course.title }</div>
-        </div>
-      </div>
-    );
-  };
-  
-  const getCourseNumber = course => (
-    course.id.slice(1, 4)
-  );
 
 export default CourseList;
